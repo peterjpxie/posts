e5b436c097b5dfe3b4847985b4f9df73edac9e20
Peter Jiping Xie <peter.jp.xie@gmail.com>
2023-10-30 06:12:00 +1100

    Update files: head_turn/detect_head_turn.py head_turn/out_obama.jpg head_turn/out_straight.jpg head_turn/out_turn_left.jpg head_turn/out_turn_right.jpg out_obama_turn_left.jpg out_obama_turn_right.jpg

--- head_turn/detect_head_turn.py
+++ head_turn/detect_head_turn.py
@@ -25,6 +25,8 @@ image = face_recognition.load_image_file(image_file)
 face_landmarks_list = face_recognition.face_landmarks(image)
 num_faces = len(face_landmarks_list)
 print(f"I found {num_faces} face(s) in this photograph.")
+# Get locations of all the faces in the image
+face_locations = face_recognition.face_locations(image)
 
 pil_image = Image.fromarray(image)
 d = ImageDraw.Draw(pil_image)
@@ -45,35 +47,32 @@ def point_distance(p1,p2):
     """get distance of two points"""
     return math.sqrt( (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 )
 
-for face_landmarks in face_landmarks_list:
-    # Print the location of each facial feature in this image
-    for facial_feature in facial_features:
-        print(
-            f"{facial_feature} has {len(face_landmarks[facial_feature])} points: {face_landmarks[facial_feature]}"
-        )
+# get landmarks of first face
+face_landmarks = face_landmarks_list[0]
+# Let's trace out each facial feature in the image with a line!
+for facial_feature in facial_features:
+    print(
+        f"{facial_feature} has {len(face_landmarks[facial_feature])} points: {face_landmarks[facial_feature]}"
+    )
+    d.line(face_landmarks[facial_feature], width=5)
 
-    # Let's trace out each facial feature in the image with a line!
-    for facial_feature in facial_features:
-        # if facial_feature == "chin":
-        d.line(face_landmarks[facial_feature], width=5)
+# draw left chin to nose top in green
+d.line(face_landmarks["chin"][-1:] + face_landmarks["nose_bridge"][:1], width=5, fill=(0, 255, 0))
+# draw right chin to nose top in red
+d.line(face_landmarks["chin"][:1] + face_landmarks["nose_bridge"][:1], width=5, fill=(255, 0, 0))
+
+# 
 
 
-    # draw left chin to nose top in green
-    d.line(face_landmarks["chin"][-1:] + face_landmarks["nose_bridge"][:1], width=5, fill=(0, 255, 0))
-    # draw right chin to nose top in red
-    d.line(face_landmarks["chin"][:1] + face_landmarks["nose_bridge"][:1], width=5, fill=(255, 0, 0))
+## Add the text to the image
+top, right, bottom, left = face_locations[0]
+position = (left, top)
+font_size = (right - left) // 6 # relative to face size
+font = ImageFont.truetype("arial.ttf", size=font_size)
+text_color = (0, 0, 255)  # RGB color
+d.text(position, 'hello', fill=text_color, font=font)
 
-    # Add the text to the image
-    # x is the position of nose top
-    position = (face_landmarks["nose_bridge"][0][0] - 100, face_landmarks["nose_bridge"][0][1] - 200)
-    # Define the font and font size
-    font = ImageFont.truetype("arial.ttf", size=60)
-
-    # Define the text color
-    text_color = (0, 0, 255)  # RGB color
-    d.text(position, 'hello', fill=text_color, font=font)
-
-# Display drawed image
+# Save drawed image
 out_file = "out_" + image_file
 print("saved to " + out_file)
 pil_image.save(out_file)
